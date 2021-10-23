e96ddec43e18debc3e915f9396c7a36c2673a8e9
rose830115 <rose830164@gmail.com>
2021-10-23 21:10:45 +0800

    Streamline code

--- app.js
+++ app.js
@@ -2,32 +2,32 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
-const restaurantList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json').results
 
 app.use(express.static('public'))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  res.render('index', { restaurants: restaurantList })
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(
+  const restaurant = restaurantList.find(
     restaurant => restaurant.id.toString() === req.params.restaurant_id
   )
-  res.render('show', { restaurant: restaurant })
+  res.render('show', { restaurant })
 })
 
 app.get('/search', (req, res) => {
   if (!req.query.keyword) {
     res.redirect("/")
   }
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant =>
-    restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const restaurants = restaurantList.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
   )
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+  res.render('index', { restaurants, keyword })
 })
 
 app.listen(port, () => {
