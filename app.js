942ff4f8e66269f0de61b96d7ffd7ac2d8ba0a55
rose830115 <rose830164@gmail.com>
2021-10-23 19:46:56 +0800

    Create search function by name

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: restaurant })
 })
 
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
 app.listen(port, () => {
   console.log(`server is running on localhost:${port}`)
 })
