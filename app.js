d8977c87d5a6fb32b64da960ac23bc7366715cf2
rose830115 <rose830164@gmail.com>
2021-10-23 20:22:21 +0800

    Create search function by category

--- app.js
+++ app.js
@@ -20,10 +20,13 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
+  if (!req.query.keyword) {
+    res.redirect("/")
+  }
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-  })
+  const restaurants = restaurantList.results.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  )
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
