8f0b70420c65e8b8921cedfdec0906125e18ea46
rose830115 <rose830164@gmail.com>
2021-10-30 17:12:45 +0800

    Add mongodb

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
+const db = mongoose.connection
 const restaurantList = require('./restaurant.json').results
+
+mongoose.connect('mongodb://localhost/restaurant-list')
+
+db.on('error',()=>{
+  console.log('mongodb error!')
+})
+
+db.once('open',()=>{
+  console.log('mongodb connected!')
+})
 
 app.use(express.static('public'))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
