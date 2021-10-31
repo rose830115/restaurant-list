6015ac6063516503c9da2b60475a0524796c5ddf
rose830115 <rose830164@gmail.com>
2021-10-31 18:16:40 +0800

    Add crate new restaurant function

--- app.js
+++ app.js
@@ -18,9 +18,11 @@ db.once('open', () => {
 })
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+//R瀏覽全部所有餐廳
 app.get('/', (req, res) => {
   return Restaurant.find()
     .lean()
@@ -28,6 +30,31 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+//R進入新增表單
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+
+//C新增餐廳
+app.post('/restaurants', (req, res) => {
+  const newRestaurant = req.body
+
+  return Restaurant.create({
+    name : newRestaurant.name,
+    name_en : newRestaurant.name_en,
+    category : newRestaurant.category,
+    image : newRestaurant.image,
+    location : newRestaurant.location,
+    phone : newRestaurant.phone,
+    google_map : newRestaurant.google_map,
+    rating : newRestaurant.rating,
+    description : newRestaurant.description
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
+//R瀏覽一家餐廳的詳細資訊
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
@@ -35,6 +62,8 @@ app.get('/restaurants/:id', (req, res) => {
     .then((restaurant) => res.render('show', { restaurant }))
     .catch(error => console.error(error))
 })
+
+
 
 app.get('/search', (req, res) => {
   if (!req.query.keyword) {
