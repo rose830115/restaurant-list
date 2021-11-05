b0ffeb898d827971e6b536e4464045413f2d18b3
rose830115 <rose830164@gmail.com>
2021-11-05 16:58:01 +0800

    Add sort function on search bar

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const port = 3000
-const restaurantList = require('./restaurant.json').results
 const routes = require('./routes')
 require('./config/mongoose')
 
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-
-//搜尋功能
-app.get('/search', (req, res) => {
-  if (!req.query.keyword) {
-    res.redirect('/')
-  }
-  const keyword = req.query.keyword.trim().toLowerCase()
-  const restaurants = restaurantList.filter(restaurant =>
-    restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
-  )
-  res.render('index', { restaurants, keyword })
-})
 
 app.listen(port, () => {
   console.log(`server is running on localhost:${port}`)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,4 +10,46 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+//搜尋功能
+router.get('/search', (req, res) => {
+  if (!req.query.keyword) {
+    res.redirect("/")
+  }
+
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const sort = req.query.sort
+  let nowSort = ''
+  switch (sort) {
+    case 'A > Z':
+      nowSort = { name: 1 }
+      break
+    case 'Z > A':
+      nowSort = { name: -1 }
+      break
+    case '類別':
+      nowSort = { category: 1 }
+      break
+    case '地區':
+      nowSort = { location: 1 }
+      break
+    default:
+      nowSort = { name: 1 }
+      break
+  }
+
+  Restaurant.find()
+    .lean()
+    .sort(nowSort)
+    .then(restaurants => {
+      restaurants = restaurants.filter(
+        restaurant =>
+          restaurant.name.toLowerCase().includes(keyword) ||
+          restaurant.category.includes(keyword)
+      )
+      res.render('index', { restaurants, keyword })
+    })
+    .catch(error => console.error(error))
+})
+
+
 module.exports = router
