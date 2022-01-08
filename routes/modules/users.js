721e359230d0ca3eda747a3c28864690a4609fa7
rose830115 <rose830164@gmail.com>
2022-01-08 15:41:55 +0800

    Add user modules/model; add login page/ register page

---
+++ routes/modules/users.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const router = express.Router()
+const User = require('../../models/user')
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+
+})
+
+module.exports = router
+
