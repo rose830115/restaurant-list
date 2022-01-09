7fa29d608a66af16120f613e62abd4ec6054d600
rose830115 <rose830164@gmail.com>
2022-01-09 14:25:30 +0800

    Add login failureFlash message

--- config/passport.js
+++ config/passport.js
@@ -8,16 +8,18 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: '此信箱尚未註冊！' })
+          req.flash('warning_msg', '此信箱尚未註冊！')
+          return done(null, false)
         }
         return bcrypt.compare(password, user.password)
           .then(isMatch => {
             if (!isMatch) {
-              return done(null, false, { message: '帳號或密碼錯誤！' })
+              req.flash('warning_msg', '帳號或密碼錯誤！')
+              return done(null, false)
             }
             return done(null, user)
           })
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,7 +10,7 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login', failureFlash: true
 }))
 
 router.get('/register', (req, res) => {
