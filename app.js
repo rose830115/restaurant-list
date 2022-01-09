bd11cfb3af6a6008a244c2cf9cb137a5eb946820
rose830115 <rose830164@gmail.com>
2022-01-09 12:09:55 +0800

    Add register function and flsh message

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -9,9 +11,21 @@ const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'mySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+//usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -17,7 +17,57 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  //檢查表填寫正確
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: '請填寫所有必填欄位！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  //檢查是否註冊過
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        errors.push({ message: '此信箱已註冊過！' })
+        return res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+        .then(() => {
+          req.flash('success_msg', '你已成功註冊！')
+          res.redirect('/users/login')
+        })
+        .catch(error => console.error(error))
+    })
+})
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '你已成功登出！')
+  res.redirect('/users/login')
 })
 
 module.exports = router
